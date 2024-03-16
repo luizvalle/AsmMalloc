72061d77123658be66f31fa2b9be21d2ecd8b40c
Luiz do Valle <lf.lemos.valle@gmail.com>
2024-03-16 12:22:01 -0400

    Create macros to interact with block headers and footers

--- src/mm.s
+++ src/mm.s
@@ -7,10 +7,59 @@
 .global mm_malloc
 .global mm_free
 
-.macro HEADER payload_addr
+// Retrieves the block size
+//
+// Parameter:
+//      addr - Address of the header or footer
+// Return:
+//      Places the result in r0
+.macro SIZE addr
+    push {r1}
+    ldr r0, [\addr]  // Load the contents of the header
+    mvn r1, #0x3  // Size mask: ~0x3
+    and r0, r0, r1  // Size of the block
+    pop {r1}
 .endm
 
+// Retrieves the bit representing whether the block is allocated
+//
+// Parameter:
+//      addr - Address of the header or footer
+// Return:
+//      Places the result in r0
+.macro ALLOCATED addr
+    push {r1}
+    ldr r0, [\addr]  // Load the contents of the header
+    mov r1, #0x1  // Allocated mask: 0x1
+    and r0, r0, r1  // Size of the block
+    pop {r1}
+.endm
+
+// Calculates the address of the block's header
+//
+// Parameter:
+//      payload_addr - Register containng the payload address
+// Return:
+//      Places the result in r0
+.macro HEADER payload_addr
+    sub r0, \payload_addr, #WORD_SIZE
+.endm
+
+// Calculates the address of the block's footer
+//
+// Parameter:
+//      payload_addr - Register containng the payload address
+// Return:
+//      Places the result in r0
 .macro FOOTER payload_addr
+    push {r1,r2}
+    mov r1, \payload_addr  // Save a copy
+    HEADER \payload_addr
+    SIZE r0
+    mov r2, #WORD_SIZE
+    sub r0, r0, r2, LSL #1  // r0 = size - 2 * WORD_SIZE
+    add r0, r1, r0  // payload + size - 2 * WORD_SIZE
+    pop {r1,r2}
 .endm
 
 .macro NEXT_PAYLOAD payload_addr
